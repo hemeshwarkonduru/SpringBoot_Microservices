a4b5f07da62eec916337ee3f109317ee479aaf54
MassKaBaap <hkonduru@iu.edu>
2024-04-14 22:24:48 -0400

    correct discovery server

--- order/src/main/java/com/everythingin/order/OrderApplication.java
+++ order/src/main/java/com/everythingin/order/OrderApplication.java
@@ -1,6 +1,7 @@
 package com.everythingin.order;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
